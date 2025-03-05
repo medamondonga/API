7130fb0f90ad62f800ec4505afd6503646e439af
Meda Mondonga <medaondonga4@gmail.com>
2025-03-06 00:44:58 +0100

    CRUD of Post

--- src/blog/urls.py
+++ src/blog/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path
-from .views import PostCreate, CategoryCreate
+from .views import PostCreate, CategoryCreate, PostList, PostDetail, PostUpdate, PostDelete
 
 urlpatterns = [
     path("category/new/", CategoryCreate.as_view(), name="category-created"),
     path("post/new/", PostCreate.as_view(), name="blog-created"),
+    path("post/list/", PostList.as_view(), name="list-post"),
+    path("post/<int:pk>/", PostDetail.as_view(), name="detail-post"),
+    path("post/update/<int:pk>/", PostUpdate.as_view(), name="update-post"),
+    path("post/delete/<int:pk>/", PostDelete.as_view(), name="delete-post")
 ]
 
--- src/blog/views.py
+++ src/blog/views.py
@@ -8,6 +8,9 @@ from rest_framework.response import Response
 from .models import Category, Post
 from .serializers import CategoySerializer, PostSerializer
 
+"""Mixins for category"""
+
+
 class CategoryCreate(CreateModelMixin, GenericAPIView):
     queryset = Category.objects.all()
     serializer_class = CategoySerializer
@@ -23,6 +26,8 @@ class CategoryCreate(CreateModelMixin, GenericAPIView):
         return response
 
 
+"""Mixins for post"""
+
 
 class PostCreate(CreateModelMixin, GenericAPIView):
     queryset = Post.objects.all()
@@ -37,3 +42,52 @@ class PostCreate(CreateModelMixin, GenericAPIView):
                 }
             )
         return response
+
+
+class PostList(ListModelMixin, GenericAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+
+    def get(self, request, *args, **kwargs):
+        return self.list(request, *args, **kwargs)
+
+
+class PostDetail(RetrieveModelMixin, GenericAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+
+    def get(self, request, *args, **kwargs):
+        return self.retrieve(request, *args, **kwargs)
+
+
+class PostUpdate(UpdateModelMixin, GenericAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+
+    def put(self, request, *args, **kwargs):
+        response = self.update(request, *args, **kwargs)
+
+        if response.status_code == status.HTTP_200_OK:
+            return Response({
+                "Message": "Article modifie avec success"
+            }, status=status.HTTP_200_OK)
+
+    def patch(self, request, *args, **kwargs):
+        response = self.partial_update(request, *args, **kwargs)
+
+        if response.status_code == status.HTTP_200_OK:
+            return Response({
+                "Message": "Article modifie avec success"
+            }, status=status.HTTP_200_OK)
+
+class PostDelete(DestroyModelMixin, GenericAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+
+    def delete(self, request, *args, **kwargs):
+        response = self.destroy(request, *args, **kwargs )
+
+        if response.status_code == status.HTTP_204_NO_CONTENT:
+            return Response({
+                "Message": "Article supprime"
+            }, status=status.HTTP_204_NO_CONTENT)
